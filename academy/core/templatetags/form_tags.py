9361b1a099569c01a578c5be496ed5d2743618c5
irfanpule <irfan.pule2@gmail.com>
2018-03-01 09:21:17 +0700

    templatetag for status display with styling

--- academy/core/templatetags/form_tags.py
+++ academy/core/templatetags/form_tags.py
@@ -1,7 +1,31 @@
 from django.template import Library
+from django.utils.safestring import mark_safe
+
+from academy.apps.students.models import Student
 
 register = Library()
 
 @register.filter(name='addclass')
 def addclass(field, class_attr):
     return field.as_widget(attrs={'class': class_attr})
+
+
+@register.filter(name='status_display')
+def status_to_display(status, styling=False):
+    if status == Student.STATUS.selection:
+        status_display = 'seleksi'
+        class_bagde = 'secondary'
+    elif status == Student.STATUS.participants:
+        status_display = 'peserta'
+        class_bagde = 'primary'
+    elif status == Student.STATUS.repeat:
+        status_display = 'mengulang'
+        class_bagde = 'warning'
+    elif status == Student.STATUS.graduate:
+        status_display = 'lulus'
+        class_bagde = 'success'
+
+    if styling:
+        return mark_safe('<span class="badge badge-%s">%s</span>' %
+                         (class_bagde, status_display))
+    return status_display
