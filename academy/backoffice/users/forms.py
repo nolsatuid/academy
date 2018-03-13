927ca6ba80a18a6231f9a35accf0709c296103bc
irfanpule <irfan.pule2@gmail.com>
2018-03-13 21:54:49 +0700

    sortir data pengguna

---
+++ academy/backoffice/users/forms.py
@@ -0,0 +1,85 @@
+import csv
+
+from io import StringIO
+from datetime import timedelta
+
+from django import forms
+from django.utils import timezone
+
+from academy.apps.accounts.models import User
+from academy.apps.students.models import Student
+from academy.core.utils import normalize_datetime_range
+from academy.core.templatetags.form_tags import get_status_student, status_to_display
+from model_utils import Choices
+
+
+class BaseFilterForm(forms.Form):
+
+    start_date = forms.DateField(
+        input_formats=["%Y-%m-%d"], label="Tanggal Mulai",
+        widget=forms.TextInput(attrs={'placeholder': 'Tanggal Mulai'}),
+    )
+    end_date = forms.DateField(
+        input_formats=["%Y-%m-%d"], label="Tanggal Akhir",
+        widget=forms.TextInput(attrs={'placeholder': 'Tanggal Akhir'}),
+    )
+    STATUS = Choices (
+        (1, 'selection', 'Seleksi'),
+        (2, 'participants', 'Peserta'),
+        (3, 'repeat', 'Mengulang'),
+        (4, 'graduate', 'Lulus')
+    )
+    status = forms.ChoiceField(choices=STATUS, required=False, label="Status")
+
+    def __init__(self, *args, **kwargs):
+        self.users = None
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if self.errors:
+            return cleaned_data
+
+        start_date = cleaned_data['start_date']
+        end_date = cleaned_data['end_date']
+
+        if cleaned_data['start_date'] > cleaned_data['end_date']:
+            self.add_error('start_date', "Tanggal mulai tidak bisa lebih dari tanggal akhir")
+
+        today = timezone.now().date()
+
+        if start_date > today:
+            self.add_error('start_date', "Tanggal mulai tidak boleh lebih hari ini")
+
+        if end_date > today:
+            self.add_error('end_date', "Tanggal Akhir tidak boleh lebih dari hari ini")
+
+        return cleaned_data
+
+    def get_data(self):
+        start_date = self.cleaned_data['start_date']
+        end_date = self.cleaned_data['end_date']
+        status = self.cleaned_data['status']
+
+        start, end = normalize_datetime_range(start_date, end_date)
+        user_ids = Student.objects.filter(status=status).distinct('user_id') \
+            .values_list('user_id', flat=True)
+        self.users = User.objects.filter(id__in=user_ids, date_joined__range=(start, end)) \
+            .exclude(is_superuser=True)
+
+        return self.users
+
+    def generate_to_csv(self):
+        csv_buffer = StringIO()
+        writer = csv.writer(csv_buffer)
+        writer.writerow([
+            'ID', 'Name', 'Email', 'No. Ponsel', 'Status'
+        ])
+
+        for user in self.users:
+            writer.writerow([
+                user.id, user.name, user.email, user.phone,
+                status_to_display(get_status_student(user))
+            ])
+        return csv_buffer
--- academy/core/utils.py
+++ academy/core/utils.py
@@ -1,3 +1,32 @@
+import datetime
+
+from django.utils import timezone
+
+
 def image_upload_path(prefix='etc'):
     path = f"images/{prefix}/%Y/%m/%d"
     return path
+
+
+def convert_to_datetime(date, tzinfo=None):
+    """
+    Converts date objects into timezone aware datetime object
+    """
+    if isinstance(date, datetime.date):
+        date = timezone.make_aware(datetime.datetime.combine(date, datetime.time()),
+                                   tzinfo or timezone.get_current_timezone())
+    return date
+
+
+def normalize_datetime_range(start, end, tzinfo=None):
+    """
+    Function to ensure both start and end are timezone aware
+    and ensure start starts at 0:0:0 and end ends at 23:59:59
+    """
+    start = convert_to_datetime(start, tzinfo)
+    end = convert_to_datetime(end, tzinfo)
+
+    start = start.replace(hour=00, minute=00, second=00)
+    end = end.replace(hour=23, minute=59, second=59)
+
+    return start, end
