a6fbc417643d0553b51242efd0f2e078c961b011
Alin Nur Alifah <alinnurkhalifah@gmail.com>
2018-03-15 23:35:20 +0700

    halaman 404 dan 500

--- academy/website/urls.py
+++ academy/website/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^home/', views.home, name='home'),
+    url(r'^error404/', views.error_404, name='error404'),
+    url(r'^error500/', views.error_500, name='error500'),
     url(r'^faq/', views.faq, name='faq'),
     url(r'^accounts/', include('academy.website.accounts.urls', namespace='accounts')),
 ]
