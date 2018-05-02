ab62b655fe6a811d835c7bf673ae062ada2f30bf
Alin Nur Alifah <alinnurkhalifah@gmail.com>
2018-05-02 13:29:53 +0700

    template dashboard

--- academy/backoffice/views.py
+++ academy/backoffice/views.py
@@ -4,8 +4,15 @@ from django.contrib.admin.views.decorators import staff_member_required
 
 @staff_member_required
 def index(request):
+    data_dashboard = {
+        'pengguna': 10,
+        'peserta': 20,
+        'pendaftar': 30
+    }
+
     context = {
-        'title': 'Dasbor'
+        'title': 'Dasbor',
+        'data': data_dashboard
     }
 
     return render(request, 'backoffice/index.html', context=context)
