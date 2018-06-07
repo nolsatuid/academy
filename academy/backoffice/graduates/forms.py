73200dfeca8452ef80f8b04e84bae7bdf71a485e
irfanpule <irfan.pule2@gmail.com>
2018-06-07 09:48:23 +0700

    send notif to participants repeat

---
+++ academy/backoffice/graduates/forms.py
@@ -0,0 +1,64 @@
+from django import forms
+from django.conf import settings
+from django.template.loader import render_to_string
+
+from academy.apps.students.models import Student, Training
+from academy.apps.accounts.models import User
+from post_office import mail
+
+
+class ParticipantsRepeatForm(forms.Form):
+    batch = forms.ModelChoiceField(
+        label='Angkatan', queryset=Training.objects.order_by('batch'), empty_label="Pilih Angkatan",
+        help_text="Pemberitahuan akan dikirim kesemua peserta tidak " \
+            "lulus pada angkatan yang dipilih"
+    )
+
+    def __init__(self, *args, **kwargs):
+        self.users_repeat = []
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        batch = self.cleaned_data['batch']
+        students = Student.objects.filter(status=Student.STATUS.participants, training=batch)
+        if not students:
+            raise forms.ValidationError("Maaf tidak ada peserta pada angkatan ini")
+
+        user_ids = students.values_list("user_id", flat=True)
+        users = User.objects.filter(id__in=user_ids)
+
+        for user in users:
+            status = user.get_count_training_status()
+            if status['graduate'] < settings.INDICATOR_GRADUATED and status['not_yet'] == 0 \
+                and status['repeat'] > 1:
+                self.users_repeat.append({'user': user, 'status': status})
+
+        if not self.users_repeat:
+            raise forms.ValidationError("Tidak ada peserta yang mengulang")
+
+        return batch
+
+    def send_notification(self):
+        title = "Maaf, Anda belum bisa lanjut"
+
+        context = {
+            'host': settings.HOST,
+            'email_title': title,
+            'indicator': settings.INDICATOR_GRADUATED
+        }
+
+        for data in self.users_repeat:
+            context['user'] = data['user']
+            context['graduate'] = data['status']['graduate']
+
+            mail.send(
+                data['user'].email,
+                settings.DEFAULT_FROM_EMAIL,
+                subject=title,
+                html_message=render_to_string('emails/participants_repeat.html', context=context)
+            )
+            student = data['user'].get_student()
+            student.status = Student.STATUS.repeat
+            student.save(update_fields=['status'])
+
+        return len(self.users_repeat)
--- academy/backoffice/graduates/urls.py
+++ academy/backoffice/graduates/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^candidates/$', views.candidates, name='candidates'),
     url(r'^candidate-to-graduates/(?P<id>\d+)$', views.candidate_to_graduates, name='candidate_to_graduates'),
     url(r'^details/(?P<id>\d+)$', views.details, name='details'),
+    url(r'^participants-repeat/$', views.participants_repeat, name='participants_repeat'),
 ]
