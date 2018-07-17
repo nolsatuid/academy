e13c87c18158f3aaa6eeac0d475c4ddffcf43804
irfanpule <irfan.biankalang@gmail.com>
2018-07-17 22:32:45 +0700

    bulk materi status

--- academy/backoffice/training_materials/forms.py
+++ academy/backoffice/training_materials/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms.formsets import BaseFormSet
 
+from academy.apps.accounts.models import User
 from academy.apps.students.models import TrainingMaterial, Training, Student, TrainingStatus
 from model_utils import Choices
 
@@ -106,3 +107,43 @@ class BaseStatusTrainingFormSet(BaseFormSet):
 
                 training_status.training_material = training_material
                 training_status.save()
+
+
+class BulkStatusTrainingForm(forms.Form):
+    STATUS = Choices(
+        (1, 'not_yet', 'Belum'),
+        (2, 'graduate', 'Lulus'),
+        (3, 'repeat', 'Ulang'),
+    )
+    status = forms.ChoiceField(choices=STATUS)
+
+    def __init__(self, student_ids=None, *args, **kwargs):
+        self.users = None
+        self.student_ids = student_ids
+        self.training_material = kwargs.pop('training_material')
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if self.errors:
+            return cleaned_data
+
+        if not self.student_ids:
+            raise forms.ValidationError("Harus memilih minimal 1 pengguna", code="no_users")
+        self.users = User.objects.filter(students__in=self.student_ids)
+        return cleaned_data
+
+    def save(self, *args, **kwargs):
+        cleaned_data = super().clean()
+        status_trainings = TrainingStatus.objects.filter(
+            user__in=self.users, training_material=self.training_material)
+
+        status_trainings.update(status=cleaned_data['status'])
+
+    def send_notification(self):
+        students = Student.objects.filter(id__in=self.student_ids)
+
+        for student in students:
+            training_materials = student.training.materials.prefetch_related('training_status')
+            student.user.notification_status_training(training_materials)
--- academy/backoffice/training_materials/views.py
+++ academy/backoffice/training_materials/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.forms import formset_factory
+from django.contrib.admin.views.decorators import staff_member_required
 
 from academy.apps.students.models import TrainingMaterial
 from .forms import (TrainingMaterialForm, StudentFilterForm, BaseStatusTrainingFormSet,
-                    ChangeStatusTraining)
+                    ChangeStatusTraining, BulkStatusTrainingForm)
 
 
+@staff_member_required
 def index(request):
     training_materials = TrainingMaterial.objects.all()
     context = {
@@ -16,6 +18,7 @@ def index(request):
     return render(request, 'backoffice/training_materials/index.html', context)
 
 
+@staff_member_required
 def add(request):
     form = TrainingMaterialForm(request.POST or None)
 
@@ -31,6 +34,7 @@ def add(request):
     return render(request, 'backoffice/form.html', context)
 
 
+@staff_member_required
 def edit(request, id):
     training_material = get_object_or_404(TrainingMaterial, id=id)
     form = TrainingMaterialForm(data=request.POST or None, instance=training_material)
@@ -47,13 +51,18 @@ def edit(request, id):
     return render(request, 'backoffice/form.html', context)
 
 
+@staff_member_required
 def delete(request, id):
     TrainingMaterial.objects.get(id=id).delete()
     messages.success(request, 'Berhasil menghapus materi pelatihan')
     return redirect('backoffice:training_materials:index')
 
 
+@staff_member_required
 def bulk_training_status(request):
+    """
+    bulk many to many change.
+    """
     form = StudentFilterForm(request.GET or None)
     formset = None
     table_heading= {}
@@ -89,3 +98,52 @@ def bulk_training_status(request):
         'table_heading': table_heading
     }
     return render(request, 'backoffice/training_materials/form-bulk-status.html', context)
+
+
+@staff_member_required
+def bulk_material_status(request):
+    form = StudentFilterForm(request.GET or None)
+    table_heading= {}
+    data = []
+    initial = {}
+    student_ids = []
+    bulk_form = None
+
+    if form.is_valid():
+        cleaned_data = form.cleaned_data
+
+        # set table heading
+        table_heading['column_user'] = f"Angkatan {cleaned_data['batch'].batch}"
+        table_heading['column_status'] = f"Status materi - {cleaned_data['training_materials'].title}"
+
+        # form bulk
+        student_ids = request.POST.getlist("students")
+        bulk_form = BulkStatusTrainingForm(
+            data=request.POST or None, student_ids=student_ids,
+            training_material=cleaned_data['training_materials']
+        )
+        if bulk_form.is_valid():
+            bulk_form.save()
+            bulk_form.send_notification()
+            messages.success(request, f"Berhasil ubah {len(student_ids)} data")
+
+        # pass data students
+        students = form.get_data()
+        for student in students:
+            materi = student.training.materials.get(id=cleaned_data['training_materials'].id)
+            data.append({
+                'id': student.id,
+                'name': student.user.name,
+                'username': student.user.username,
+                'email': student.user.email,
+                'status': materi.get_training_status(student.user).status
+            })
+
+    context = {
+        'title': 'Status Pelatihan',
+        'form': form,
+        'students': data,
+        'table_heading': table_heading,
+        'bulk_form': bulk_form
+    }
+    return render(request, 'backoffice/training_materials/status-list.html', context)
