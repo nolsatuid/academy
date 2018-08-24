f8afa56675abbd31114e09a442736fbcee488e31
Alin Nur Alifah <alinnurkhalifah@gmail.com>
2018-08-24 22:53:46 +0700

    update magament command email failed

--- academy/core/management/commands/retry_email_failed.py
+++ academy/core/management/commands/retry_email_failed.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from django.core.management.base import BaseCommand
 
 from post_office.models import Email, STATUS
@@ -7,7 +9,9 @@ class Command(BaseCommand):
     help = 'Change status email failed to queued to resend'
 
     def handle(self, *args, **options):
-        emails = Email.objects.filter(status=STATUS.failed)
-        failed_email = emails.count()
+        now = timezone.now()
+
+        emails = Email.objects.filter(status=STATUS.failed, created__lt=now.date())   
+        failed = emails.count()
         emails.update(status=STATUS.queued)
-        print('%s email success change status to queued' % failed_email)
+        print('%s email success change status to queued' % failed)
