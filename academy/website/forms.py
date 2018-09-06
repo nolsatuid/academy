2fb2565662da32cc437f1cfd9ea1c450f99c014a
Alin Nur Alifah <alinnurkhalifah@gmail.com>
2018-09-06 10:14:39 +0700

    update query get sertifikat

--- academy/website/forms.py
+++ academy/website/forms.py
@@ -6,16 +6,15 @@ from academy.apps.graduates.models import Graduate
 class CertificateVerifyForm(forms.Form):
     certificate_number = forms.CharField(label='Nomor Sertifikat', max_length=18,
                                 widget=forms.TextInput(attrs={'placeholder': 'ex: NS-XXXXX-XXXX-XXXX'}))
-    last_name = forms.CharField(label='Nama Belakang pada Sertifikat')
+    last_name = forms.CharField(label='Nama Belakang')
 
     def verification(self, *args, **kwargs):
         certificate_number = self.cleaned_data['certificate_number']
         last_name = self.cleaned_data['last_name']        
-        student = Graduate.objects.filter(certificate_number__iexact=certificate_number).select_related('user')
-        if student:
-            if student.last().user.last_name:
-                student = student.filter(user__last_name__iexact=last_name)
+        graduates = Graduate.objects.filter(certificate_number__iexact=certificate_number).select_related('user').last()
+        if graduates:
+            if (graduates.user.last_name and graduates.user.last_name.lower() == last_name.lower()) or \
+                (graduates.user.first_name and graduates.user.first_name.lower() == last_name.lower()):
+                return graduates
             else:
-                student = student.filter(user__first_name__iexact=last_name)
-
-        return student.last()
+                return None
