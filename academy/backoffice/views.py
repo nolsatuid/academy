3e0852f1287435909ce545bdf162788c4e2c4ab1
Alin Nur Alifah <alinnurkhalifah@gmail.com>
2018-10-15 20:09:22 +0700

    update grafik dashboard

--- academy/backoffice/views.py
+++ academy/backoffice/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
 
 from django.db.models import Count, Q
+from django.db import connection
 from academy.apps.accounts.models import User
 from academy.apps.students.models import Student, Training
 
@@ -14,15 +15,19 @@ def index(request):
     }
 
     angkatan = Training.objects.order_by('batch')
+    truncate_date = connection.ops.date_trunc_sql('month', 'date_joined')
+    users = User.objects.exclude(is_superuser=True).exclude(is_staff=True).extra({'month':truncate_date})
+    users_month = users.values('month').annotate(Count('id')).order_by('month')
+    
     context = {
         'title': 'Dasbor',
         'data': data_dashboard,
         'angkatan': angkatan,
-        'jumlah_pendaftar': angkatan.annotate(num_students=Count('students')),
-        'jumlah_peserta': angkatan.filter(students__status = Student.STATUS.participants) \
-            .annotate(num_participants=Count('students')),
+        'jumlah_pendaftar': users_month,
         'jumlah_lulus': angkatan.filter(students__status = Student.STATUS.graduate) \
-            .annotate(num_graduate=Count('students'))
+            .annotate(num_graduate=Count('students')),
+        'jumlah_ulang': angkatan.filter(students__status = Student.STATUS.repeat) \
+            .annotate(num_repeat=Count('students'))
     }
-
+    
     return render(request, 'backoffice/index.html', context=context)
