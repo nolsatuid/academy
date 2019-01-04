1505a03dab2bddf0d099c0e651c188307f6990c4
Alin Nur Alifah <alinnurkhalifah@gmail.com>
2019-01-04 14:32:06 +0700

    grafik fix

--- academy/backoffice/views.py
+++ academy/backoffice/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
 
 from django.db.models import Count, Q
+from django.db.models.functions import Coalesce
 from django.db import connection
 from academy.apps.accounts.models import User
 from academy.apps.students.models import Student, Training
@@ -19,16 +20,14 @@ def index(request):
     truncate_date = connection.ops.date_trunc_sql('month', 'date_joined')
     users = User.objects.exclude(is_superuser=True).exclude(is_staff=True).extra({'month':truncate_date})
     users_month = users.values('month').annotate(Count('id')).order_by('month')
-    
+
     context = {
         'title': 'Dasbor',
         'data': data_dashboard,
         'angkatan': angkatan,
         'jumlah_pendaftar': users_month,
-        'jumlah_lulus': angkatan.filter(students__status = Student.STATUS.graduate) \
-            .annotate(num_graduate=Count('students')),
-        'jumlah_ulang': angkatan.filter(students__status = Student.STATUS.repeat) \
-            .annotate(num_repeat=Count('students'))
+        'jumlah_lulus': angkatan.annotate(num_graduate=Coalesce(Count('students', filter=Q(students__status=Student.STATUS.graduate)),0) or 0),
+        'jumlah_ulang': angkatan.annotate(num_repeat=Coalesce(Count('students', filter=Q(students__status=Student.STATUS.repeat)),0) or 0)
     }
     
     return render(request, 'backoffice/index.html', context=context)
