e55344b0cdb98a2eac10c6a0c182158f949a615a
Alin Nur Alifah <alinnurkhalifah@gmail.com>
2019-01-24 10:45:35 +0700

    change url to django 2.1 path

--- academy/backoffice/graduates/urls.py
+++ academy/backoffice/graduates/urls.py
@@ -1,21 +1,21 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = 'graduates'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^candidates/$', views.candidates, name='candidates'),
-    url(r'^candidate-to-graduates/(?P<id>\d+)$', views.candidate_to_graduates, name='candidate_to_graduates'),
-    url(r'^details/(?P<id>\d+)$', views.details, name='details'),
-    url(r'^participants-repeat/$', views.participants_repeat, name='participants_repeat'),
-    url(r'^status-training/(?P<id>\d+)/$', views.status_training,
+    path('', views.index, name='index'),
+    path('candidates/', views.candidates, name='candidates'),
+    path('candidate-to-graduates/<int:id>', views.candidate_to_graduates, name='candidate_to_graduates'),
+    path('details/<int:id>', views.details, name='details'),
+    path('participants-repeat/', views.participants_repeat, name='participants_repeat'),
+    path('status-training/<int:id>/', views.status_training,
         name='status_training'),
-    url(r'^certificate/(?P<id>\d+)/$', views.show_certificate,
+    path('certificate/<int:id>/', views.show_certificate,
         name='show_certificate'),
-    url(r'^add-training-material/(?P<id>\d+)/$', views.add_training_material,
+    path('add-training-material/<int:id>/', views.add_training_material,
         name='add_training_material'),
-    url(r'^delete-training-status/(?P<id>\d+)/(?P<graduate_id>\d+)$', views.delete_training_status,
+    path('delete-training-status/<int:id>/<int:graduate_id>', views.delete_training_status,
         name='delete_training_status'),
 ]
--- academy/backoffice/training_materials/urls.py
+++ academy/backoffice/training_materials/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = 'training_materials'
 
 urlpatterns = [
-    url(r'^(?P<id>\d+)/delete/$', views.delete, name='delete'),
-    url(r'^(?P<id>\d+)/edit/$', views.edit, name='edit'),
-    url(r'^add/$', views.add, name='add'),
-    url(r'^$', views.index, name='index'),
-    url(r'^status/$', views.bulk_material_status, name='bulk_material_status')
+    path(r'<int:id>/delete/', views.delete, name='delete'),
+    path(r'<int:id>/edit/', views.edit, name='edit'),
+    path(r'add/', views.add, name='add'),
+    path(r'', views.index, name='index'),
+    path(r'status/', views.bulk_material_status, name='bulk_material_status')
 ]
