514448c3ec6a02df572a4a28c9f01306bbe3cc0b
Alin Nur Alifah <alinnurkhalifah@gmail.com>
2019-03-18 15:49:10 +0700

    update flow test case

--- tests/test_auth_user.py
+++ tests/test_auth_user.py
@@ -2,6 +2,9 @@ from django.test import TestCase
 from django.urls import reverse
 
 from academy.apps.accounts.models import User
+from academy.apps.surveys.model import Survey
+
+from academy.website.accounts.forms import SurveyForm
 
 
 class AuthUserTest(TestCase):
@@ -12,6 +15,21 @@ class AuthUserTest(TestCase):
         url = user.generate_auth_url()
         response = self.client.get(url)
         if hasattr(user, 'survey'):
+            print("Has Survey")
             self.assertRedirects(response, '/accounts/survey/edit/', status_code=302, target_status_code=200, fetch_redirect_response=True)
         else:
+            print("Not Yet")
             self.assertRedirects(response, '/accounts/survey/', status_code=302, target_status_code=200, fetch_redirect_response=True)
+            self.create_survey(user)
+            self.test_edit_survey()
+
+    def create_survey(self, user):
+        #create survey
+        data = {
+            'working_status': Survey.WORKING_STATUS_CHOICES.employee,
+            'graduate_channeled': True,
+            'graduate_channeled_when': Survey.GRADUATE_CHANNELED_TIME_CHOICES.soon
+        }
+        form = SurveyForm(data=data)
+        self.assertTrue(form.is_valid())
+        form.save(user)
