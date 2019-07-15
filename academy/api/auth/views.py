fd180b83576f7586f4d917a85e4f71ce0166c178
irfanpule <irfan.pule2@gmail.com>
2019-07-15 16:33:11 +0700

    refactor auth login, can enter email username

---
+++ academy/api/auth/views.py
@@ -0,0 +1,34 @@
+from academy.website.accounts.forms import CustomAuthenticationForm
+from academy.api.response import ErrorResponse
+from academy.api.serializers import user_profile
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .forms import APIRegisterForm
+
+
+class AuthLogin(APIView):
+    def post(self, request):
+        form = CustomAuthenticationForm(request, data=request.data)
+        if form.is_valid():
+            user = form.get_user()
+            refresh = RefreshToken.for_user(user)
+            data = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'user': user_profile(user)
+            }
+            return Response(data)
+        return ErrorResponse(form)
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        form = APIRegisterForm(request.data)
+        if form.is_valid():
+            user = form.save()
+            return Response({'message': f'Mohon cek email {user.email} untuk mengaktifkan akun Anda'})
+
+        return ErrorResponse(form)
--- academy/api/urls.py
+++ academy/api/urls.py
@@ -4,15 +4,10 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView, TokenVerifyView
 )
 
-from . import views
-
 
 app_name = "api"
 urlpatterns = [
-    path('auth/login', TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path('auth/token-refresh', TokenRefreshView.as_view(), name="token_refresh"),
-    path('auth/token-verify', TokenVerifyView.as_view(), name="token_verify"),
-    path('auth/register', views.RegisterView.as_view(), name="register"),
+    path('auth/', include('academy.api.auth.urls')),
     path('user/', include('academy.api.user.urls')),
     path('infos/', include('academy.api.infos.urls')),
 ]
