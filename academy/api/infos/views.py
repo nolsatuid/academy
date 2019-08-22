f50a50efd8d43843da27f386c92092062b94ee60
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2019-08-22 22:29:03 +0700

    api verification

--- academy/api/infos/views.py
+++ academy/api/infos/views.py
@@ -1,11 +1,15 @@
+from datetime import timedelta
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from academy.api.response import ErrorResponse
 from academy.apps.accounts.models import User
 from academy.apps.students.models import Student
 from academy.apps.graduates.models import Graduate
 from academy.apps.offices.models import LogoPartner, LogoSponsor
 from academy.api import serializers
+from academy.website.forms import CertificateVerifyForm
 
 
 class GetStatisticsView(APIView):
@@ -32,3 +36,24 @@ class GetLogoPartners(APIView):
 
 class GetLogoSponsors(GetLogoPartners):
     logos = LogoSponsor.objects.filter(is_visible=True).order_by('display_order')
+
+
+class VerifyCertificate(APIView):
+    def post(self, request):
+        form = CertificateVerifyForm(request.data or None)
+        if form.is_valid():
+            student = form.verification()
+            if student:
+                valid_date = student.created + timedelta(days=1095)
+                context = {
+                    'pemilik': f'{student.user.first_name} {student.user.last_name}',
+                    'nomor_sertifikat': student.certificate_number,
+                    'tanggal_sertifikat': student.created.strftime('%d %b %Y'),
+                    'berlaku_hingga': valid_date.strftime('%d %b %Y')
+                }
+                return Response(context)
+
+            return ErrorResponse(error_message='Maaf, kami tidak dapat menemukan sertifikat dengan '
+                                               'nomor sertifikat dan nama belakang tersebut.')
+
+        return ErrorResponse(form)
