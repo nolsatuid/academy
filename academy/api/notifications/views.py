be56b1d4e3b8a6085a7ce3b9293dec917b656d27
alinnural <alin@btech.co.id>
2020-01-19 20:16:46 +0700

    add validation and change parameter user

--- academy/api/notifications/views.py
+++ academy/api/notifications/views.py
@@ -1,17 +1,26 @@
 from rest_framework.response import Response
 
+from django.utils.datastructures import MultiValueDictKeyError
+
 from academy.api.authentications import UserAuthAPIView
 from academy.api.response import ErrorResponse
-from academy.apps.accounts.models import Inbox
+from academy.apps.accounts.models import Inbox, User
 
 
 class SendNotification(UserAuthAPIView):
     def post(self, request):
         data = request.data
-        inbox = Inbox.objects.create(user=request.user, subject=data["title"], content=data["content"])
 
-        if inbox :
+        try :
+            user = User.objects.get(id=data["user_id"])
+        except User.DoesNotExist :
+            return ErrorResponse(error_message='User tidak tersedia')
+
+        try :
+            inbox = Inbox.objects.create(user=user, subject=data["title"], content=data["content"])
             return Response({'message': 'Pesan berhasil disimpan'})
+        except MultiValueDictKeyError:
+            return ErrorResponse(error_message='Parameter tidak sesuai')
         
         return ErrorResponse(error_message='Pesan gagal disimpan')
 
