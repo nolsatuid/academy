eee29c56fdb0996b7b913223ffb7156d693a9f9f
irfanpule <irfan.pule2@gmail.com>
2020-01-21 10:05:06 +0700

    add field color style

--- academy/apps/offices/models.py
+++ academy/apps/offices/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from academy.core.utils import image_upload_path
 from ckeditor.fields import RichTextField
+from model_utils import Choices
 
 
 class LogoPartner(models.Model):
@@ -29,6 +30,17 @@ class LogoSponsor(models.Model):
 class BannerInfo(models.Model):
     title = models.CharField(max_length=150)
     content = RichTextField(help_text="Tuliskan informasi yang akan ditampilkan.")
+    COLOR_STYLE = Choices(
+        ('success', 'Success'),
+        ('danger', 'Danger'),
+        ('warning', 'Warning'),
+        ('primary', 'Primary'),
+        ('info', 'Info')
+    )
+    color_style = models.CharField(
+        max_length=50, choices=COLOR_STYLE,
+        default=COLOR_STYLE.success
+    )
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
 
