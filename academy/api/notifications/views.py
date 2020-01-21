871c5edd28c985be64b0d1a881cad25337749423
alinnural <alin@btech.co.id>
2020-01-21 10:11:19 +0700

    fix code validation

--- academy/api/notifications/views.py
+++ academy/api/notifications/views.py
@@ -4,25 +4,17 @@ from django.utils.datastructures import MultiValueDictKeyError
 
 from academy.api.authentications import UserAuthAPIView
 from academy.api.response import ErrorResponse
-from academy.apps.accounts.models import Inbox, User
 
+from .forms import AddInboxForm
 
 class SendNotification(UserAuthAPIView):
-    def post(self, request):
-        data = request.data
-
-        try :
-            user = User.objects.get(id=data["user_id"])
-        except User.DoesNotExist :
-            return ErrorResponse(error_message='User tidak tersedia')
-
-        try :
-            inbox = Inbox.objects.create(user=user, subject=data["title"], content=data["content"])
+    def post(self, request):      
+        form = AddInboxForm(request.data)
+        if form.is_valid():
+            form.save()
             return Response({'message': 'Pesan berhasil disimpan'})
-        except MultiValueDictKeyError:
-            return ErrorResponse(error_message='Parameter tidak sesuai')
-        
-        return ErrorResponse(error_message='Pesan gagal disimpan')
+
+        return ErrorResponse(form)
 
         
         
