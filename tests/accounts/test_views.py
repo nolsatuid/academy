22cd9c4b81dfb41bf6f296aba13fea4f9eebd772
irfanpule <irfan.pule2@gmail.com>
2020-07-07 15:17:32 +0700

    fix error tests

--- tests/accounts/test_views.py
+++ tests/accounts/test_views.py
@@ -1,23 +1,19 @@
-from django.test import TestCase
-from django.test import Client
+from tests import AcademyTestCase
 
 from academy.apps.accounts.models import User, Inbox
-from academy.apps.offices.models import Setting, AuthSetting
 
 
-class AccountsUserViewTest(TestCase):
+class AccountsUserViewTest(AcademyTestCase):
     fixtures = ['accounts.json', 'students.json']
 
     def setUp(self):
-        Setting.objects.create()
-        AuthSetting.objects.create()
+        super().setUp()
+        self.user = User.objects.get(email='user2@gmail.com')
+        self.user.set_password('password123')
+        self.user.save()
 
     def test_inbox(self):
-        user = User.objects.get(email='user2@gmail.com')
-        user.set_password('password123')
-        user.save()
-
-        login = self.client.login(username=user.username, password='password123')
+        login = self.client.login(username=self.user.username, password='password123')
         self.assertTrue(login)
 
         # normal url
